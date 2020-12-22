2e5d19b538cad707de0fdd9e4d23c8b91fc2f270
Pavana sri vinaya <pavanasri.nyros@gmail.com>
2020-12-22 19:58:48 +0530

    first

---
+++ config.js
@@ -0,0 +1,3 @@
+export default {
+  api_key: "4df5929526ee8f600cde14f868027693"
+};
---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="about">
+    <span>
+      In this App We can search for the images. In this app I have used the
+      Flickr API
+      <br />
+      <h5>Author : Pavana sri</h5>
+      <a href="https://www.flickr.com/services/api/"
+        >Flickr API Link that i used to in this app to display the data</a
+      >
+    </span>
+  </v-container>
+</template>
